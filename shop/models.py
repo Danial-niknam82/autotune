9ef36afc094998f8a15c05b3026564e8970e17e2
Danial Niknam <danialniknam82@gmail.com>
2025-01-05 14:46:26 +0330

    about us and home page

--- shop/models.py
+++ shop/models.py
@@ -30,6 +30,9 @@ class Product(models.Model):
   category = models.ForeignKey(Category , on_delete=models.CASCADE, default = 1)
   picture = models.ImageField(upload_to='upload/product/')
 
+  is_sale = models.BooleanField(default=False)
+  sale_price = models.DecimalField(default=0, decimal_places=0 , max_digits=12)
+
   def __str__(self):
     return self.name
      
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,7 @@ from . models import Product
 def helloworld(request):
   all_products = Product.objects.all()
   return render(request , 'index.html',{'products': all_products})
+
+
+def about(request):
+  return render(request , "about.html")
