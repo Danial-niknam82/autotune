d65d4b0d5fec4957d46f62b3c21c94c8d64bda5f
Danial Niknam <danialniknam82@gmail.com>
2025-01-12 16:43:08 +0330

    see item option

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('about/' , views.about , name = 'about'),
     path('login/', views.login_user ,name = 'login'),
     path('logout/', views.logout_user ,name = 'logout'),
-    path('signup', views.signup_user,name = "signup")
+    path('signup/', views.signup_user,name = "signup"),
+    path('product/<int:pk>/', views.product, name="product"),
+    path('category/<str:cat>/', views.category, name="category")
+
 ]
